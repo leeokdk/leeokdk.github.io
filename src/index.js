77c175553a966343d82bd5d127463aa8ddb5e30e
lee ok <88550876+leeokdk@users.noreply.github.com>
2021-10-29 17:07:58 +0900

    add and updates

--- src/index.js
+++ src/index.js
@@ -1,7 +1,31 @@
-/*
-실시간 시계
-로컬 스토리지를 사용한 로그인
-로컬 스토리지를 사용한 투두리스트
-랜덤 배경 이미지
-날씨와 위치 (geolocation)
-*/
+/*랜덤 배경 이미지*/
+const imgs = [
+    "laurent-mandine",
+    "eleanor-styles",
+    "ben-stern",
+    "dan-meyers",
+    "finde-zukunft",
+    "marvin-meyer",
+    "gabrielle-henderson",
+    "sarah-takforyan",
+    "divani-diva",
+    "emma-ou",
+    "joanna-kosinska",
+    "marie-thibault",
+    "daniil-komov",
+    "aaron-burden",
+    "ben-allan",
+    "ffa801",
+    "dayne-topkin"
+];
+
+//스크롤바 생기는 이슈 해결
+function setScreenSize() {
+  let vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
+};
+setScreenSize();
+document.body.style = 'height: --vh;';
+
+const chosenImage = imgs[Math.floor(Math.random() * imgs.length)];
+document.body.style = `background-image: url(img/${chosenImage}.jpg)`;
---
+++ src/login.js
@@ -0,0 +1,36 @@
+/* 로컬 스토리지를 사용한 로그인*/
+const loginForm = document.querySelector("#login-form");
+const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
+
+const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
+
+function onLoginSubmit(event) {
+  event.preventDefault();
+  loginForm.classList.add(HIDDEN_CLASSNAME);
+  const username = loginInput.value;
+  localStorage.setItem(USERNAME_KEY, username);
+  paintGreetings(username);
+}
+
+function paintGreetings(username) {
+  greeting.innerText = `Hello ${username}`;
+  greeting.classList.remove(HIDDEN_CLASSNAME);
+  addOutBtn();
+}
+
+function addOutBtn(){
+  const outbtn = document.createElement('button');
+  outbtn.setAttribute.innerText = 'Log Out';
+  greeting.appendChild(outbtn);
+}
+
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if (savedUsername === null) {
+  loginForm.classList.remove(HIDDEN_CLASSNAME);
+  loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+  paintGreetings(savedUsername);
+}
